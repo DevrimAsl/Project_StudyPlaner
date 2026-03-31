48f9be440d6ffd0d20f91dfee93059cc471c0d3f
Devrim Aslan <devrim.aslan@bulme.at>
2026-03-31 15:24:46 +0200

    updated urls.py

--- mystudyplaner/basic/urls.py
+++ mystudyplaner/basic/urls.py
@@ -3,7 +3,14 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('add-subject/', views.add_subject, name='add_subject'),
+    path('add-task/', views.add_task, name='add_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
 ]
